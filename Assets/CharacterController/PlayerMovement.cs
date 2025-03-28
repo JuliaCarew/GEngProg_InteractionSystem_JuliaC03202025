15cb67aca55696ed8ea727e37bb19697b1514180
JuliaCarew <crewjulia3@gmail.com>
2025-03-28 12:34:54 -0300

    updated scene management, finalized

--- Assets/CharacterController/PlayerMovement.cs
+++ Assets/CharacterController/PlayerMovement.cs
@@ -5,17 +5,20 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private float moveSpeed = 5.0f;
     private Rigidbody2D playerRigidbody;
-    private Animator playerAnimator;
+    public Animator playerAnimator;
     private Vector2 moveDirection;
     private bool canMove = true;
     
     void Awake()
     {
+        playerAnimator = this.GetComponentInChildren<Animator>();
         playerRigidbody = this.GetComponent<Rigidbody2D>();
-        playerAnimator = this.GetComponent<Animator>();
         InputActions.MoveEvent += UpdateMoveVector;
     }
-
+    private void Update()
+    {
+        HandleAnim();
+    }
     private void UpdateMoveVector(Vector2 inputVector) // player input = moveVector(current vector2 from HandlePlayerMove)
     {
         if (canMove == true){
@@ -41,6 +44,25 @@ public class PlayerMovement : MonoBehaviour
         if (state == false) 
         {
             moveDirection = Vector2.zero; // stop player movement when not allowed to move
+        }
+    }
+
+    /// <summary>
+    /// Swap between animations in the blend tree based on 
+    /// horizontal/vertical input vector. Handles idle state as well.
+    /// </summary>
+    private void HandleAnim() 
+    {
+        Vector2 movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        if (movement.magnitude != 0)
+        {
+            playerAnimator.SetFloat("Horizontal", movement.y);
+            playerAnimator.SetFloat("Vertical", movement.x);
+            playerAnimator.SetBool("Moving",true);
+        }
+        else {
+            playerAnimator.SetBool("Moving", false);
         }
     }
 
